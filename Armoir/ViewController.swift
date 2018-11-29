fb64e8adb2b5645f155278b549e70724a731d2dd
asweitzman <asweitzman@gmail.com>
2018-11-29 10:59:57 -0800

    Initial Commit

---
+++ Armoir/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Armoir
+//
+//  Created by alex weitzman on 11/29/18.
+//  Copyright © 2018 CS147. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
