ff944ed68c25c45378fc3ab3ebdbb438567a4b9d
Rachel <rhyon@stanford.edu>
2018-12-03 21:11:08 -0800

    newsviewcontroller

---
+++ Armoir/NewsTableViewCell.swift
@@ -0,0 +1,72 @@
+//
+//  NewsTableViewCell.swift
+//  Armoir
+//
+//  Created by Rachel Hyon on 12/2/18.
+//  Copyright © 2018 CS147. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell{
+    
+
+    @IBOutlet weak var productImg: UIImageView!
+ 
+    
+    
+    /*@IBOutlet weak var profileImg: UIImageView!
+    
+    @IBOutlet weak var productImg: UIImageView!
+    
+    @IBOutlet weak var profile: UILabel!
+    
+    @IBOutlet weak var distance: UILabel!
+    
+    @IBOutlet weak var message: UILabel!*/
+    
+    /*var productImg: UIImage?
+    var profileImg: UIImage?
+    var profile: String?
+    var distance: String?
+    var message: String?
+
+    
+    var productImgView : UIImageView = {
+        var imageView = UIImageView ()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    var profileImgView : UIImageView = {
+        var imageView = UIImageView ()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    var profileView : UITextView = {
+        var textView = UITextView ()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+    
+    var distanceView: UITextView = {
+        var textView = UITextView ()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+    
+    var messageView: UITextView = {
+        var textView = UITextView ()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder) has not been implemented")
+    }*/
+}
