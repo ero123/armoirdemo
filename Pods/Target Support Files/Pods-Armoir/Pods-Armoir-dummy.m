052454e5c76b0268e334aa532788b438e0120ef0
asweitzman <asweitzman@gmail.com>
2018-12-07 04:17:05 -0800

    Added cocoapods

---
+++ Pods/Target Support Files/Pods-Armoir/Pods-Armoir-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Armoir : NSObject
+@end
+@implementation PodsDummy_Pods_Armoir
+@end
---
+++ Pods/Target Support Files/Pods-ArmoirTests/Pods-ArmoirTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ArmoirTests : NSObject
+@end
+@implementation PodsDummy_Pods_ArmoirTests
+@end
